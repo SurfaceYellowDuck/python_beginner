4303bbe10a6ecf3b2cb3945f64ae68ab6ad09491
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2023-09-11 23:26:50 +0300

    Changed the logic of the program to a more correct one by condition.

--- ex_32.py
+++ ex_32.py
@@ -1,8 +1,10 @@
-a = [1, 3, 15, 25]
-a_1 = 1
-a_2 = 16
-ind_list = []
-for i in range(len(a)):
-    if a_1 <= i <= a_2:
-        ind_list.append(i - 1)
-print(ind_list)
+def fun():
+    a = [1, 3, 15, 25]
+    a_1 = int(input("Input num one"))
+    a_2 = int(input("Input num two"))
+    ind_list = []
+    for i in range(len(a)):
+        if a_1 <= a[i] <= a_2:
+            ind_list.append(i)
+    return ind_list
+print(fun())
